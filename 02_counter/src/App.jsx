aa69388bd481273a81e7dac7ae26121704e22b8f
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-11-27 14:01:15 +0530

    useState hook - add counter added till 20 - remove till 0

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -3,16 +3,25 @@ import "./App.css";
 
 function App() {
 
-  let [counter, setCounter] = useState(15)
+  let [counter, setCounter] = useState(0)
 
   // let counter = 5;
 
   const addValue = () =>{
     // console.log("Value Added", Math.random());
-    console.log(counter);
+    // console.log(counter);
     // counter = counter + 1;
+    if(counter < 20){
     setCounter(counter + 1);
+    }
   }
+
+  const removeValue = () =>{
+    if(counter > 0){
+    setCounter(counter - 1);
+    }
+  }
+
   return (
     <>
       <h1>Chai Aur react</h1>
@@ -20,7 +29,7 @@ function App() {
       <button onClick={addValue}>Add Value</button>
       <br />
       <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   );
 }
