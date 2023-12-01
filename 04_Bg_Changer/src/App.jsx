6b9895f34f3cb4d5a882a08cf2ffaf0de19cc386
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-01 23:58:29 +0530

    Adding background change functionality

--- 04_Bg_Changer/src/App.jsx
+++ 04_Bg_Changer/src/App.jsx
@@ -10,7 +10,35 @@ function App() {
         style={{ backgroundColor: color }}
       ></div>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl"> Text </div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+          <button
+            onClick={() => {
+              SetColor("red");
+            }}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => {
+              SetColor("green");
+            }}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => {
+              SetColor("blue");
+            }}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
       </div>
     </>
   );
