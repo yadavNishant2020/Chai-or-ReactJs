834bba4a673109a21dea1813539b68b8a432560d
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-11 21:45:01 +0530

    Currency Converter project setup

---
+++ 06_currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl bg-orange-500'>Currency Ap</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06_currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  return (
+    useEffect(() => {
+        fetch
+    }, [])
+    )
+}
+
+export default useCurrencyInfo
