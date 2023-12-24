5035cd07a4d67cd00929f8aa63ce63c50cb11662
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-25 01:26:14 +0530

    Refactored auth.js: handled all error - Introduced Services module for handling database operations and file management - includes methods for creating, updating, deleting, retrieving, and listing posts. - File upload, delete, and preview methods

--- 12_Blog_Nebula/src/appwrite/auth.js
+++ 12_Blog_Nebula/src/appwrite/auth.js
@@ -13,6 +13,30 @@ export class AuthService {
     this.account = new Account(this.client);
   }
 
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw new Error("Failed to login. Please check your email and password.");
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw new Error("Failed to get current user.");
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw new Error("Failed to logout.");
+    }
+  }
+
   async createAccount({ email, password, name }) {
     try {
       const userAccount = await this.account.create(
@@ -22,37 +46,18 @@ export class AuthService {
         name
       );
       if (userAccount) {
-        //call another method
         return this.login({ email, password });
       } else {
-        return;
+        throw new Error("Failed to create user account.");
       }
     } catch (error) {
-      throw error;
-    }
-  }
-
-  async login({ email, password }) {
-    try {
-      return await this.account.createEmailSession(email, password);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async getCurrentUser() {
-    try {
-      return await this.account.get();
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async logout() {
-    try {
-      await this.account.deleteSessions();
-    } catch (error) {
-      throw error;
+      if (error.code === 401) {
+        throw new Error("Invalid credentials. Please check your email and password.");
+      } else if (error.code === 409) {
+        throw new Error("User account already exists with the provided email.");
+      } else {
+        throw new Error("An error occurred while creating the user account.");
+      }
     }
   }
 }
---
+++ 12_Blog_Nebula/src/appwrite/conf.js
@@ -0,0 +1,133 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Services {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost(title, slug, content, featuredImage, status, userId) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            );
+        } catch (error) {
+            console.error("Error creating post:", error);
+            throw new Error("Failed to create post.");
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            console.error("Error updating post:", error);
+            throw new Error("Failed to update post.");
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            console.error("Error getting post:", error);
+            throw new Error("Failed to get post.");
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            console.error("Error getting posts:", error);
+            throw new Error("Failed to get posts.");
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.error("Error uploading file:", error);
+            throw new Error("Failed to upload file.");
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(config.appwriteBucketId, fileId);
+            return true;
+        } catch (error) {
+            console.error("Error deleting file:", error);
+            throw new Error("Failed to delete file.");
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+            return true;
+        } catch (error) {
+            console.error("Error deleting post:", error);
+            throw new Error("Failed to delete post.");
+        }
+    }
+
+    async previewFile(fileId) {
+        try {
+            return await this.bucket.getFilePreview(
+                config.appwriteBucketId,
+                fileId
+            );
+        } catch (error) {
+            console.error("Error previewing file:", error);
+            throw new Error("Failed to preview file.");
+        }
+    }
+}
+
+const service = new Services();
+export default service;
