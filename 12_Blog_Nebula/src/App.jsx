86e63218c1a1e365a638069681c8cb1d2f4fe5d8
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-25 13:12:26 +0530

    Redux Toolkit Configuration - Component and Page Structure - Tailwind Setup

--- 12_Blog_Nebula/src/App.jsx
+++ 12_Blog_Nebula/src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/authSlice";
+import { Header, Footer } from "./components";
+import "./App.css";
+// import { Outlet } from "react-router-dom";
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((useData) => {
+        if (useData) {
+          dispatch(login({ useData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
