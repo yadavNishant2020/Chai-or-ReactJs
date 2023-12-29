baebc52dad7690ed0ee8649d2243f8a8c3ea6be1
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-29 23:55:52 +0530

    - Login component - Sign Up component - Authentication Layout

--- 12_Blog_Nebula/src/components/SignUp.jsx
+++ 12_Blog_Nebula/src/components/SignUp.jsx
@@ -1,32 +1,36 @@
 import { useState } from "react";
+import authService from "../appwrite/auth";
 import { Link, useNavigate } from "react-router-dom";
-import { login as storeLogin } from "../features/authSlice";
+import { login } from "../features/authSlice";
 import { Button, Input, Logo } from "./index";
 import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 
-function Login() {
+function SignUp() {
   const navigate = useNavigate();
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
-  const [error, setError] = useState("");
 
-  const login = async (data) => {
+  const create = async (data) => {
     setError("");
     try {
-      const session = await authService.login(data);
-      if (session) {
-        const userData = await authService.getCurrentUser();
-        if (userData) dispatch(storeLogin(userData));
-        navigate("/");
+      const userData = await authService.createAccount(data);
+      if (data) {
+        if (userData) {
+          const userData = await authService.getCurrentUser();
+          if (userData) {
+            dispatch(login(userData));
+            navigate("/");
+          }
+        }
       }
     } catch (error) {
       setError(error.message);
     }
   };
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -36,20 +40,28 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
+            <Input
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
             <Input
               label="Email:"
               placeholder="Enter your email"
@@ -58,21 +70,20 @@ function Login() {
                 required: true,
                 validate: {
                   matchPatern: (value) =>
-                    /^([\w.-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) ||
+                    /^([\w.-_]+)?\w+@[\w-_]+(\.\w+){1,}$/gim.test(value) ||
                     "Email is invalid",
                 },
               })}
             />
             <Input
-              label="Password:"
+              label="password: "
+              tyoe="password"
               placeholder="Enter your password"
-              type="password"
               {...register("password", {
                 required: true,
-                minLength: 6,
               })}
             />
-            <Button type="submit" className="w-full">Sign In</Button>
+            <Button type="submit" className="w-full"></Button>
           </div>
         </form>
       </div>
@@ -80,4 +91,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default SignUp;
